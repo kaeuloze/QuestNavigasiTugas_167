2a022a13eac365cc3fae8e913dd2ec49a4cdaa51
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-08 00:22:54 +0700

    menambahkan composable FormulirPendaftaranScreen

---
+++ app/src/main/java/com/example/navigastugas/view/FormPendaftaran.kt
@@ -0,0 +1,22 @@
+package com.example.navigasiku.view
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormulirPendaftaranScreen(
+    onBackToHome: () -> Unit
+) {}
